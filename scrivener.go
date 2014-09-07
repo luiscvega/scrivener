b165f432adaa231a412de15ed40742c0e7c57c80
Luis Vega <luiscvega@gmail.com>
2014-09-07 21:00:54 +0800

    Removed old code

--- scrivener.go
+++ scrivener.go
@@ -17,20 +17,3 @@ func New(s interface{}) *scrivener {
 
 	return form
 }
-
-func (scrivener *scrivener) AssertPresent(fieldName string) {
-	result := scrivener.elem.FieldByName(fieldName).String()
-
-	if result == "" {
-		scrivener.Errors = append(scrivener.Errors, fieldName+" can't be blank!")
-	}
-}
-
-func (scrivener *scrivener) AssertEqual(fieldName1 string, fieldName2 string) {
-	result1 := scrivener.elem.FieldByName(fieldName1).String()
-	result2 := scrivener.elem.FieldByName(fieldName2).String()
-
-	if result1 != result2 {
-		scrivener.Errors = append(scrivener.Errors, fieldName1+" and "+fieldName2+" do not match!")
-	}
-}
