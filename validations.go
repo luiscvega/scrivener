b96f41083b16ced42fe00b50609a2134a8001759
Luis Vega <luis@glamdring.local>
2014-09-07 23:16:57 +0800

    Assertions return boolean

--- validations.go
+++ validations.go
@@ -1,22 +1,24 @@
 package scrivener
 
-func (scrivener *scrivener) Assert(fn func(interface{}) bool, key string, message string) {
-	result := fn(scrivener.s)
-	if !result {
+func (scrivener *scrivener) Assert(fn func(interface{}) bool, key string, message string) bool {
+	pass := fn(scrivener.s)
+	if !pass {
 		scrivener.Errors[key] = append(scrivener.Errors[key], message)
+		return false
 	}
+	return true
 }
 
-func (scrivener *scrivener) AssertPresent(fieldName string) {
-	scrivener.Assert(func(s interface{}) bool {
+func (scrivener *scrivener) AssertPresent(fieldName string) bool {
+	return scrivener.Assert(func(s interface{}) bool {
 		value := scrivener.elem.FieldByName(fieldName).String()
 
 		return len(value) > 0
 	}, fieldName, "not_present")
 }
 
-func (scrivener *scrivener) AssertEqualString(fieldName string, expected string) {
-	scrivener.Assert(func(s interface{}) bool {
+func (scrivener *scrivener) AssertEqualString(fieldName string, expected string) bool {
+	return scrivener.Assert(func(s interface{}) bool {
 		actual := scrivener.elem.FieldByName(fieldName).String()
 
 		return actual == expected
